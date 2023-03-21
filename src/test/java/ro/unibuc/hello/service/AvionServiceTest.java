11d7464ea2e94a41a047dd9b4634dd81e1f662b4
Ana <anamares10@gmail.com>
2023-03-21 22:04:14 +0200

    Added AvionServiceTest class and tests for getAvionInfoByNumber

---
+++ src/test/java/ro/unibuc/hello/service/AvionServiceTest.java
@@ -0,0 +1,57 @@
+package ro.unibuc.hello.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ro.unibuc.hello.data.Avion;
+import ro.unibuc.hello.data.AvionRepository;
+import ro.unibuc.hello.dto.InfoAvion;
+import ro.unibuc.hello.exception.EntityNotFoundException;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class AvionServiceTest {
+    @Mock
+    AvionRepository mockAvionRepository;
+    @InjectMocks
+    AvionService avionService = new AvionService();
+    private static final String avionTemplate = "%s : %s -> %s";
+
+
+    @Test
+    void test_getAvionInfoByNumber_returnsInfoAvion(){
+        // Arrange
+        String number = "1";
+        Avion avion = new Avion("1", "Doha", "Bangkok");
+        InfoAvion infoAvion = new InfoAvion(String.format(avionTemplate, avion.number, avion.from, avion.to));
+
+        when(mockAvionRepository.findByNumber(number)).thenReturn(avion);
+
+        // Act
+        InfoAvion resInfoAvion = avionService.getAvionInfoByNumber(number);
+
+        // Assert
+        Assertions.assertEquals(infoAvion.getFlight(), resInfoAvion.getFlight());
+    }
+
+    @Test
+    void test_getAvionInfoByNumber_whenAvionNotFound() {
+        // Arrange
+        String number = "1";
+
+        when(mockAvionRepository.findByNumber(number)).thenReturn(null);
+
+        try {
+            // Act
+            InfoAvion infoAvion = avionService.getAvionInfoByNumber(number);
+        } catch (Exception ex) {
+            // Assert
+            Assertions.assertEquals(ex.getClass(), EntityNotFoundException.class);
+            Assertions.assertEquals(ex.getMessage(), "Entity: 1 was not found");
+        }
+    }
+}
