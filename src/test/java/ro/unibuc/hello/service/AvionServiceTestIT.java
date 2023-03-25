c2f6697c647451883b5e976201765c5d54d4639c
Ana <anamares10@gmail.com>
2023-03-25 14:41:28 +0200

    small tag fix

--- src/test/java/ro/unibuc/hello/service/AvionServiceTestIT.java
+++ src/test/java/ro/unibuc/hello/service/AvionServiceTestIT.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
-//@Tag("IT")
+@Tag("IT")
 public class AvionServiceTestIT {
     @Autowired
     AvionRepository avionRepository;
