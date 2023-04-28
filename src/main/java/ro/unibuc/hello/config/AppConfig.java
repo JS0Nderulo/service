cf7f09b1377ac8e124209354370d30e52b9606e4
Ana <anamares10@gmail.com>
2023-04-28 13:58:55 +0300

    Try to add proxy for e2e tests

--- src/main/java/ro/unibuc/hello/config/AppConfig.java
+++ src/main/java/ro/unibuc/hello/config/AppConfig.java
@@ -2,13 +2,25 @@ package ro.unibuc.hello.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 @Configuration
 public class AppConfig {
 
     @Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+    String  host = "localhost";
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        InetSocketAddress address = new InetSocketAddress(host, 8080);
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, address);
+        factory.setProxy(proxy);
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(factory);
+        return restTemplate;
     }
 }
